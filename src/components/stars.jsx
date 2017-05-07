15683be8adc03bb0369840014e1dc82558e57bda
Saurabh Bayani <saurabh.bayani@gmail.com>
2017-05-07 18:10:55 +0530

    Random Number Generation

--- src/components/stars.jsx
+++ src/components/stars.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 
 class StarsTile extends React.Component {
+    constructor(props) {
+        super(props);
+        this.starsCount = Math.floor(Math.random() * 9) + 1;
+        this.starsDom = [];
+    }
+
     render() {
+        for(let i=0; i<this.starsCount; i++) {
+            this.starsDom.push(
+                <span className='glyphicon glyphicon-star star' key={i}></span>
+            )
+        }
         return (
             <div className='well stars-tile'>
-
+                {this.starsDom}
             </div>
         )
     }
